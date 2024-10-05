6d5943286ac97ae7f44dd4490080c494405d0ec5
romain.cescutti <romain.cescutti@gagarine.efrits.fr>
2024-10-05 17:25:31 +0200

    Added butcher.h any function that concerns deleting any in-game material should be linked with butcher.h
    added "orphan" detector as pixel delete

---
+++ src/butcher/butcher.c
@@ -0,0 +1,21 @@
+#include "butcher.h"
+
+void butcher_add(t_deltimer pix, t_deletelist list)
+{
+  list.pixels[list.len] = pix;
+  list.len +=1;
+}
+
+void butcher(map map,game game)
+{
+  while (i < len && i < NBRCELL)
+    {
+      if (list.pixels[i].timer > frames)
+	{
+	  pixel_delete(map,list.pixels[i].pos);
+	  memcpy(&list.pixels[i],&list.pixels[len],sizeof(list.pixels[len]));
+	  len -1;
+	}
+      i++;
+    }
+}
